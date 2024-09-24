b31a96daecedb2fec459b34f4a960ed323ef6feb
Mridul299 <midulislam820@gmail.com>
2024-09-24 15:37:31 +0600

    addeda modal

--- js/index.js
+++ js/index.js
@@ -2,18 +2,20 @@ document.getElementById('donate-noakhali').addEventListener('click',function(){
     const totaldonated = accountbalance('donate-amount');
     const donatevalue =  inputvalue("donate-value");
     const myBalance = accountbalance("my-balance");
+    const modal = document.getElementById("modal");
     if(donatevalue >= 0 && donatevalue !== isNaN('donate-value') ){
         const donatedbalance = donatevalue + totaldonated;      
         document.getElementById("donate-value").value = '';
         if(donatevalue > myBalance){
-            alert('you have not enought money for donate');
+            alert('you have not enought money for donate');        
             return;
         }
         else{
             const availablebalance = myBalance - donatevalue;
             document.getElementById('my-balance').innerText = availablebalance;              
         };
-        document.getElementById("donate-amount").innerText = donatedbalance;         
+        document.getElementById("donate-amount").innerText = donatedbalance;
+        modal.showModal();         
     }
     else{
         alert('please input valid value');
@@ -26,6 +28,7 @@ document.getElementById("donate-feni").addEventListener('click',function(){
     const totaldonated = accountbalance('donate-amount-1');
     const donatevalue =  inputvalue("donate-value-1");
     const myBalance = accountbalance("my-balance"); 
+    const modal = document.getElementById("modal");
     if(donatevalue >= 0 && donatevalue !== isNaN('donate-value-1')){
         const donatedbalance = donatevalue + totaldonated;      
         document.getElementById("donate-value-1").value = '';     
@@ -38,6 +41,7 @@ document.getElementById("donate-feni").addEventListener('click',function(){
             document.getElementById('my-balance').innerText = availablebalance;               
         };   
         document.getElementById('donate-amount-1').innerText = donatedbalance; 
+        modal.showModal();    
     }
     else{
         alert('please input valid value');
@@ -51,6 +55,7 @@ document.getElementById("donate-students").addEventListener('click',function(){
     const totaldonated = accountbalance('donate-amount-2');
     const donatevalue =  inputvalue("donate-value-2");
     const myBalance = accountbalance("my-balance");
+    const modal = document.getElementById("modal");
     if(myBalance <0 ){
         alert('you have not enough money for donate');
         return;
@@ -66,10 +71,12 @@ document.getElementById("donate-students").addEventListener('click',function(){
             const availablebalance = myBalance - donatevalue;
             document.getElementById('my-balance').innerText = availablebalance;             
         };
-        document.getElementById('donate-amount-2').innerText = donatedbalance;    
+        document.getElementById('donate-amount-2').innerText = donatedbalance;
+        modal.showModal();     
     }
     else{
         alert('please input valid value');
     }
 });
+
 
