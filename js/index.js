e4134edcb762d815f2460752687cf6253ee2556d
Mridul299 <midulislam820@gmail.com>
2024-09-24 19:42:34 +0600

    added a new card

--- js/index.js
+++ js/index.js
@@ -67,10 +67,6 @@ document.getElementById("donate-students").addEventListener('click',function(){
     const donatevalue =  inputvalue("donate-value-2");
     const myBalance = accountbalance("my-balance");
     const modal = document.getElementById("modal");
-    if(myBalance <0 ){
-        alert('you have not enough money for donate');
-        return;
-    };
     if(donatevalue >= 0 && donatevalue !== isNaN('donate-value-2')){
         const donatedbalance = donatevalue + totaldonated;   
         document.getElementById("donate-value-2").value = '';     
@@ -83,6 +79,38 @@ document.getElementById("donate-students").addEventListener('click',function(){
             document.getElementById('my-balance').innerText = availablebalance;             
         };
         document.getElementById('donate-amount-2').innerText = donatedbalance;
+        modal.showModal();
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <div class="border-2 p-8 max-w-6xl m-auto rounded-2xl">
+            <h1 class="font-bold">${donatevalue} Taka is Donated for Aid for Injured in the Quota Movement, Bangladesh</h1>
+            <p class="text-slate-700"> Date : ${Date()} </p>
+        </div>
+        `;
+        document.getElementById('history-container').appendChild(div);      
+    }
+    else{
+        alert('please input valid value');
+    }
+});
+
+document.getElementById("donate-palestine").addEventListener('click',function(){
+    const totaldonated = accountbalance('donate-amount-3');
+    const donatevalue =  inputvalue("donate-value-3");
+    const myBalance = accountbalance("my-balance");
+    const modal = document.getElementById("modal");
+    if(donatevalue >= 0 && donatevalue !== isNaN('donate-value-2')){
+        const donatedbalance = donatevalue + totaldonated;   
+        document.getElementById("donate-value-3").value = '';     
+        if(donatevalue > myBalance){
+            alert('you have not enough money for donate');
+            return;
+        }
+        else{
+            const availablebalance = myBalance - donatevalue;
+            document.getElementById('my-balance').innerText = availablebalance;             
+        };
+        document.getElementById('donate-amount-3').innerText = donatedbalance;
         modal.showModal();
         const div = document.createElement('div');
         div.innerHTML = `
